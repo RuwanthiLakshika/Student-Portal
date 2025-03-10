25ce619479be716f14028755133b557d2a5334cf
Ruwanthi Lakshika <125971277+RuwanthiLakshika@users.noreply.github.com>
2025-03-10 13:08:53 +0530

    changes related to home page

--- StudentPortal/StudentPortal/obj/Debug/net8.0/StudentPortal.AssemblyInfo.cs
+++ StudentPortal/StudentPortal/obj/Debug/net8.0/StudentPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9126469891db0792b5f7abd264d79e1c3a4af22b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bac8b2c900ba3af4cd358bc6b37f0a88fcbc130")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
